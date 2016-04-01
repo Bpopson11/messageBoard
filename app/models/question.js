c2ac882ee0168e6a37faf8cf34d7605fb97a40ac
Bri <bpopson@gmail.com>
2016-04-01 08:48:58 -0700

    add hardcoded data to index.js

--- app/models/question.js
+++ app/models/question.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  query:
-  info:
-  author:
-  date:
+  query: DS.attr(),
+  info: DS.attr(),
+  author: DS.attr(),
+  date: DS.attr()
 });
