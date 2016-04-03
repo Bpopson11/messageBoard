dbd259f1e6bdf399d6b029aebea0c1bcad896481
Bri <bpopson@gmail.com>
2016-04-03 16:03:39 -0700

    set up answer model

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+  });
   },
   actions: {
     update(question, params) {
