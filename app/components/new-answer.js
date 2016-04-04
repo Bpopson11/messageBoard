cfdacc0a1f476a5d4846a0dfce503c08e951dc55
Bri <bpopson@gmail.com>
2016-04-04 12:06:39 -0700

    connect new answers to questions -ps

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -10,6 +10,7 @@ export default Ember.Component.extend({
       var params = {
         response: this.get('response'),
         name: this.get('name'),
+        question: this.get('question'),
       };
       this.set('addNewAnswer', false);
       this.sendAction('saveAnswer2', params);
--- app/routes/question.js
+++ app/routes/question.js
@@ -5,8 +5,13 @@ export default Ember.Route.extend({
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
-    destroyQuestion(question) {
-      question.destroyRecord();
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
       this.transitionTo('index');
     },
     saveAnswer3(params) {
@@ -17,15 +22,10 @@ export default Ember.Route.extend({
         return question.save();
       });
       this.transitionTo('question', params.question);
-  },
-    update(question, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          question.set(key,params[key]);
-        }
-      });
-      question.save();
-      this.transitionTo('index');
     },
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
