4efabb3a55134019d59d48160f320547683d6490
Gibberish314 <a.moosen@gmail.com>
2016-04-14 09:32:12 -0700

    refactor helper with foreach loop.

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -3,14 +3,23 @@ import Ember from 'ember';
 export function questionPopularity(params) {
   var question = params[0];
 
-  if(question.get('answers').get('length') >= 4) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span>');
-  } else if (question.get('answers').get('length') >= 3) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span>');
-  } else if (question.get('answers').get('length') >= 2) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign">');
-  } else if (question.get('answers').get('length') > 0) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-question-sign"></span>');
-  }
+  // if(question.get('answers').get('length') >= 4) {
+  //   return Ember.String.htmlSafe('<span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span>');
+  // } else if (question.get('answers').get('length') >= 3) {
+  //   return Ember.String.htmlSafe('<span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign"></span>');
+  // } else if (question.get('answers').get('length') >= 2) {
+  //   return Ember.String.htmlSafe('<span class="glyphicon glyphicon-question-sign"></span><span class="glyphicon glyphicon-question-sign">');
+  // } else if (question.get('answers').get('length') > 0) {
+  //   return Ember.String.htmlSafe('<span class="glyphicon glyphicon-question-sign"></span>');
+  // }
+  var outputGlyphs = ""
+  question.get('answers').forEach(function(answer, index){
+    if (index > 4) {
+      break;
+    } else {
+      outputGlyphs += '<span class="glyphicon glyphicon-question-sign"></span>';
+    }
+  })
+  return outputGlyphs;
 }
 export default Ember.Helper.helper(questionPopularity);
